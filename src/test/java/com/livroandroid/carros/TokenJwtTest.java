fe63e258aaf3fd05c5bf46b19c691d9fdff58f5f
tiago <tiago.pereira.vieira@gmail.com>
2020-07-29 20:58:27 -0300

    Package update

--- src/test/java/com/livroandroid/carros/TokenJwtTest.java
+++ src/test/java/com/livroandroid/carros/TokenJwtTest.java
@@ -1,7 +1,7 @@
 package com.livroandroid.carros;
 
 
-import com.livroandroid.carros.api.security.jwt.JwtUtil;
+import com.livroandroid.carros.api.infra.security.jwt.JwtUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
