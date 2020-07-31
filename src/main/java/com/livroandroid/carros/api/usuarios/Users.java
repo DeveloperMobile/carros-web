cc7a9af38a3e8ee0802d0688dd9281a5cb18f082
tiago <tiago.pereira.vieira@gmail.com>
2020-07-31 20:46:29 -0300

    branch hpostgres update 5 Class Users update

--- src/main/java/com/livroandroid/carros/api/usuarios/Users.java
+++ src/main/java/com/livroandroid/carros/api/usuarios/Users.java
@@ -22,7 +22,7 @@ public class Users implements UserDetails {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            joinColumns = @JoinColumn(name = "users_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles;
 
