e95e0410f723a4b007a6e1a4264403755609ecac
tiago <tiago.pereira.vieira@gmail.com>
2020-07-31 20:51:47 -0300

    branch hpostgres update 6

--- src/main/java/com/livroandroid/carros/api/usuarios/Users.java
+++ src/main/java/com/livroandroid/carros/api/usuarios/Users.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
-
+// Update
 @Entity
 @Data
 public class Users implements UserDetails {
